495d6c029ef5098ba9014b7f47e3d0d17cc26d19
dsc <dieter@schwarenthorer.at>
2019-04-14 01:14:34 +0200

    Convert to Azure

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# K8s Spot Termination Handler
+# K8s Azure Low-priority Preemption Termination Handler
+
+This work is based on K8s Spot AWS Termination Handler for draining spot instances on AWS EC2.
 
 ## Table of contents
 * [Introduction](#introduction)
@@ -10,17 +12,17 @@
 
 ## Introduction
 
-The K8s Spot Termination handler watches the AWS metadata service when running on Spot Instances.
+The K8s Azure Spot Termination handler watches the Azure scheduled events service when running on Low-priority Instances.
 
-When a Spot Instance is due to be terminated, precisely 2 minutes before it's
+When a Low Priority Instance is due to be terminated, precisely 30 secons before it's
 termination a "termination notice" is issued.
-The K8s Spot Termination Handler watches for this and then gracefully drains the
+The K8s Azure Spot Termination Handler watches for this and then gracefully drains the
 node it is running on before the node is taken away by AWS.
 
 ## Usage
 
 ### Deploy to Kubernetes
-A docker image is available at `quay.io/pusher/k8s-spot-termination-handler`.
+A docker image is available at `docker.io/diseq/k8s-azspot-termination-handler`.
 These images are currently built on pushes to master. Releases will be tagged as and when releases are made.
 
 Sample Kubernetes manifests are available in the [deploy](deploy/) folder.
@@ -29,19 +31,20 @@ To deploy in clusters using RBAC, please apply all of the manifests (Daemonset, 
 
 #### Requirements
 
-For the K8s Termination Handler to schedule correctly; you will need an identifying label on your spot instances.
+For the K8s Termination Handler to schedule correctly; you will need an identifying label on your low-priority instances.
 
-We add a label `node-role.kubernetes.io/spot-worker` to our spot instances and hence this is the default value in the node selector of the [Daemonset](deploy/daemonset.yaml).
+We add a label `node-role.kubernetes.io/azspot-worker` to our low-priority instances and hence this is the default value in the node selector of the [Daemonset](deploy/daemonset.yaml).
 ```yaml
 nodeSelector:
-  "node-role.kubernetes.io/spot-worker": "true"
+  "node-role.kubernetes.io/azspot-worker": "true"
 ```
 To achieve this, add the following flag to your Kubelet:
 ```
---node-labels="node-role.kubernetes.io/spot-worker=true"
+--node-labels="node-role.kubernetes.io/azspot-worker=true"
 ```
 
 ## Related
+- [K8s Spot AWS Termination Handler](https://github.com/pusher/k8s-spot-termination-handler): K8s AWS Spot Preemption Termination Handler
 - [K8s Spot Rescheduler](https://github.com/pusher/k8s-spot-rescheduler): Move nodes from on-demand instances to spot instances when space is available.
 
 ## Communication
--- docker-entrypoint.py
+++ docker-entrypoint.py
@@ -1,38 +1,61 @@
-from subprocess import check_output, call
+import json
 from requests import get
 from os import getenv
+import socket
+import sys
 from time import sleep
 
+this_host = socket.gethostname()
 
 if __name__ == '__main__':
-    print('Starting up')
 
-    node_name = getenv('NODE_NAME')
+  print('Starting up')
 
-    print('Watching for termination notice on node %s' % node_name)
+  node_name = getenv('NODE_NAME')
 
-    counter = 0
+  print('Watching for termination notice on node %s' % node_name)
 
-    while(True):
-        response = get(
-            "http://169.254.169.254/latest/meta-data/spot/termination-time"
-        )
-        if response.status_code == 200:
-            kube_command = ['kubectl', 'drain', node_name,
-                            '--grace-period=120', '--force',
-                            '--ignore-daemonsets']
+  counter = 0
 
-            print("Draining node: %s" % node_name)
-            result = call(kube_command)
-            if (result == 0):
-                print('Node Drain successful')
-                break
+  while(True):
 
-        else:
-            if counter == 60:
-                counter = 0
-                print("Termination notice status: %s, on Node: %s" %
-                      (response.status_code, node_name)
-                      )
-            counter += 5
-            sleep(5)
+    response = get(
+      "http://169.254.169.254/metadata/scheduledevents?api-version=2017-11-01",
+      headers={'Metadata': 'true'}
+    )
+
+    if response.status_code == 200:
+      data = response.json()
+
+      for evt in data['Events']:
+          eventid = evt['EventId']
+          status = evt['EventStatus']
+          resources = evt['Resources']
+          eventtype = evt['EventType']
+          resourcetype = evt['ResourceType']
+          notbefore = evt['NotBefore'].replace(" ","_")
+
+          if this_host in resources:
+              print("+ Scheduled Event. This host " + this_host + " is scheduled for " + eventtype + " not before " + notbefore)
+
+              # time we have for drain is depending on event (https://docs.microsoft.com/en-us/azure/virtual-machines/linux/scheduled-events)
+              # in case of Preempt just 30 secs
+              kube_command = ['kubectl', 'drain', node_name,
+                              '--grace-period=0', '--force',
+                              '--ignore-daemonsets']
+
+              print("Draining node: %s" % node_name)
+              result = call(kube_command)
+              if (result == 0):
+                  print('Node Drain successful')
+                  break
+
+    if counter == 60:
+        counter = 0
+        print("Scheduled events status: %s, on Node: %s" %
+              (response.status_code, node_name))
+
+    counter += 5
+    sleep(5)
+
+  sys.exit(0)
